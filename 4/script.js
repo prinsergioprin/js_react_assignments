08525c4f288618570514eaeb84999e2038cd3872
prinsergioprin <sergioprin24@gmail.com>
2025-01-08 01:23:22 +0100

    Complete assignment 4

--- 4/script.js
+++ 4/script.js
@@ -6,5 +6,22 @@ buttonContainer.addEventListener("click", (event) => {
 });
 
 buttonChildContainer.addEventListener("click", (event) => {
-  alert("Child container clicked!");
+  event.stopPropagation();
+  if (event.target.matches("button")) {
+    alert(`${event.target.textContent} clicked`);
+  } else {
+    alert("Child container clicked!");
+  }
+});
+
+const button2 = document.getElementById("button2");
+button2.onclick = function (event) {
+  event.stopPropagation();
+  alert("Button 2 clicked");
+};
+
+const button3 = document.getElementById("button3");
+button3.addEventListener("click", (event) => {
+  event.stopPropagation();
+  alert("Button 3 clicked");
 });
